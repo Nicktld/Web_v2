3af63f3c009580226094d741cf4fcb5e2a155e2e
Ledong <tangledong2016@gmail.com>
2019-01-02 16:43:01 -0500

    handers updated

--- www/handlers.py
+++ www/handlers.py
@@ -4,14 +4,27 @@
 __author__ = 'Leon Tang' 
 
 ' url handlers '
-
+import time
 from coroweb import get
-from models import User
+from models import User, Blog
 
-@get('/')
-async def index(request):
+@get('/users')
+async def all_users(request):
     users = await User.findAll()
     return {
         '__template__': 'test.html',
         'users': users
+    }
+
+@get('/')
+async def index(request):
+    summary = 'No pains, no gains. With talents, great success!'
+    blogs = [
+        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+        Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
+        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
+        ]
+    return {
+        '__template__': 'blogs.html',
+        'blogs': blogs
     }
