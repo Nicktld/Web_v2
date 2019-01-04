e28b607634f374c4f0362bbe975c45340147a863
Ledong <tangledong2016@gmail.com>
2019-01-04 16:06:53 -0500

    Manage Page Added

--- www/handlers.py
+++ www/handlers.py
@@ -8,13 +8,26 @@ __author__ = 'Leon Tang'
 import re, time, logging, hashlib, base64, asyncio, json
 import markdown2
 from aiohttp import web
-from apis import APIValueError, APIResourceNotFoundError
+from apis import APIValueError, APIResourceNotFoundError, APIPermissionError
 from config import configs
 from coroweb import get, post
 from models import User, Blog, Comment, next_id
 
 COOKIE_NAME = 'webv2'
 _COOKIE_KEY = configs.session.secret
+
+def check_admin(request):
+    if request.__user__ is None or not request.__user__.admin:
+        raise APIPermissionError('Permission Denied! Please login as an administrator')
+
+def get_page_index(page_str):
+    p = 1
+    try:
+        p = int(page_str)
+    except ValueError as e:
+        pass
+    p = max(1, p)
+    return p 
 
 def user2cookie(user, max_age):
     '''
@@ -25,6 +38,10 @@ def user2cookie(user, max_age):
     sh = '%s-%s-%s-%s' % (user.id, user.passwd, expires, _COOKIE_KEY)
     L = [user.id, expires, hashlib.sha1(sh.encode('utf-8')).hexdigest()]
     return '-'.join(L)
+
+def text2html(text):
+    lines = map(lambda s: '<p>%s</p>' % s.replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;'), filter(lambda s: s.strip() != '', text.split('\n')))
+    return ''.join(lines)
 
 async def cookie2user(cookie_str):
     '''
@@ -52,6 +69,7 @@ async def cookie2user(cookie_str):
         logging.exception(e)
         return None
 
+#############################
 @get('/users')
 async def all_users(request):
     users = await User.findAll()
@@ -59,6 +77,7 @@ async def all_users(request):
         '__template__': 'test.html',
         'users': users
     }
+############################
 
 @get('/')
 async def index(request):
@@ -71,8 +90,21 @@ async def index(request):
     return {
         '__template__': 'blogs.html',
         'blogs': blogs,
-        '__user__': request.__user__
+        # '__user__': request.__user__
+        # The statement above only remains the logged-in status on the index. We should render __user__ at the middlewares so that users can stay logged in every page.
     }
+@get('/blog/{id}')
+async def get_blog(id):
+    blog = await Blog.find(id)
+    comments = await Comment.findAll('blog_id=?', [id], orderBy='created_at desc')
+    for c in comments:
+        c.html_content = text2html(c.content)
+    blog.html_content = markdown2.markdown(blog.content)
+    return {
+        '__template__': 'blog.html',
+        'blog': blog,
+        'comments': comments
+    } 
 
 @get('/register')
 def register():
@@ -127,17 +159,25 @@ async def api_get_users():
         u.passwd = '******'
     return dict(users=users)
 
+@get('/manage/blogs/create')
+def manage_create_blog():
+    return {
+        '__template__': 'manage_blog_edit.html',
+        'id': '',
+        'action': '/api/blogs'
+    }
+
 _RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$')
 _RE_SHA1 = re.compile(r'^[0-9a-f]{40}$')
 
 @post('/api/users')
 async def api_register_user(*, email, name, passwd):
     if not name or not name.strip():
-        raise APIValueError('name')
+        raise APIValueError('name', 'name cannot be empty')
     if not email or not _RE_EMAIL.match(email):
-        raise APIValueError('email')
+        raise APIValueError('email', 'email cannot be empty or invalid')
     if not passwd or not _RE_SHA1.match(passwd):
-        raise APIValueError('passwd')
+        raise APIValueError('passwd', 'password cannot be empty or invalid')
     uid = next_id()
     sha1_passwd = '%s:%s' % (uid, passwd)
     user = User(id=uid, name=name.strip(), email=email, passwd=hashlib.sha1(sha1_passwd.encode('utf-8')).hexdigest(), image = 'about:blank')
@@ -149,3 +189,21 @@ async def api_register_user(*, email, name, passwd):
     r.content_type = 'application/json'
     r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
     return r
+
+@get('/api/blogs/{id}')
+async def api_get_blog(*, id):
+    blog = await Blog.find(id)
+    return blog
+
+@post('/api/blogs')
+async def api_create_blog(request, *, name, summary, content):
+    check_admin(request)
+    if not name or not name.strip():
+        raise APIValueError('name', 'name cannot be empty')
+    if not summary or not summary.strip(): 
+        raise APIValueError('summary', 'summary cannot be empty')
+    if not content or not content.strip(): 
+        raise APIValueError('content', 'content cannot be empty')
+    blog = Blog(user_id=request.__user__.id, user_name=request.__user__.name, user_image=request.__user__.image, name=name.strip(), summary=summary.strip(), content=content.strip()) 
+    await blog.save()
+    return blog
